a6251b08b57f7f2c93cb4ee635ed89fca65dd19e
Kehali B. Woldemichael <kehaliwoldemichael@protonmail.com>
2023-11-09 06:11:13 -0500

    feat: added code for setting up encrypted volumes

--- gentoo-1.sh
+++ gentoo-1.sh
@@ -23,12 +23,12 @@ set -e
 GENTOO_MIRROR="http://distfiles.gentoo.org"
 GENTOO_ARCH="amd64"
 GENTOO_STAGE3="amd64"
+GRUB_PLATFORMS=pc
 
 TARGET_DISK=/dev/vda
-TARGET_BOOT_SIZE=1G
-TARGET_ROOT_SIZE=24G
-
-GRUB_PLATFORMS=pc
+TARGET_BOOT_SIZE=1GiB
+TARGET_ROOT_SIZE=15GiB
+# Home will be rest of remaining space 
 
 echo "### Checking configuration..." # Can add later
 
@@ -38,17 +38,32 @@ chronyd -q
 echo "### Creating partitions..."
 parted -s -a optimal ${TARGET_DISK} \
   mklabel gpt \
-  mkpart primary 0% 1GiB \
+  mkpart primary 0% ${TARGET_BOOT_SIZE} \
   mkpart primary 1GiB 100% \
-
 echo "### Formatting partitions..."
 yes | mkfs.vfat -F 32 ${TARGET_DISK}1
-yes | mkfs.xfs ${TARGET_DISK}2
+yes | mkfs.xfs -f ${TARGET_DISK}2
 
-echo "### Labeling partitions..."
-e2label ${TARGET_DISK}1 boot
-e2label ${TARGET_DISK}2 volgroup0
+echo "### Setting up encrypted physical volume"
+cryptsetup luksFormat /dev/vda2
+cryptsetup open --type luks /dev/vda2 lvm
+pvcreate --dataalignment 1m /dev/mapper/lvm
+vgcreate vg0 /dev/mapper/lvm
+echo "### Creating logical volumes for home/root within encrypted physical volume..."
+lvcreate -L ${TARGET_ROOT_SIZE} vg0 -n lv-root 
+lvcreate -l 100%FREE vg0 -n lv-home
+echo "### Activating home/root logic volumes..."
+modprobe dm_mod # Load necessary modules into memory
+vgscan # Check that kernel realizes that we just read in an LVM 
+yes | vgchange -ay # Activate logical volume groups
+echo "### Setting filesystem for root/home logical volumes..."
+yes | mkfs.xfs /dev/vg0/lv_root
+yes | mkfs.xfs /dev/vg0/lv_home
 
 echo "### Mounting partitions..."
-mkdir -p /mnt/gentoo/boot && mount ${TARGET_DISK}1 /mnt/gentoo/boot
-mkdir -p /mnt/gentoo && mount ${TARGET_DISK}2 /mnt/gentoo
+mkdir -p /mnt/boot && mount ${TARGET_DISK}1 /mnt/boot
+mkdir -p /mnt/root && mount /dev/vg0/lv_root /mnt/home
+mkdir -p /mnt/home && mount /dev/vg0/lv-home /mnt/boot
+
+
+
