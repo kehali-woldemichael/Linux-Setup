be699e3a383659c2ca3dd2a56398adb14b4080e6
Kehali B. Woldemichael <kehaliwoldemichael@protonmail.com>
2023-12-21 08:36:51 -0500

    Update gentoo-1.sh

--- gentoo-1.sh
+++ gentoo-1.sh
@@ -33,12 +33,12 @@ TARGET_ROOT_SIZE=20GiB
 TARGET_SWAP_SIZE=10GiB
 # Home will be rest of remaining space 
 
-echo "\n### Checking configuration..." # Can add later
-
-echo "\n### Setting time..."
+echo "\n"
+echo "### Setting time..."
 chronyd -q
 
-echo "\n### Creating partitions..."
+echo "\n"
+echo "### Creating partitions..."
 parted -s -a optimal ${TARGET_DISK} \  mklabel gpt \
   mkpart primary 0% ${TARGET_BOOT_SIZE} \
   mkpart primary 1GiB 100% \
@@ -46,37 +46,43 @@ echo "### Formatting partitions..."
 yes | mkfs.vfat -F 32 ${TARGET_DISK}1
 yes | mkfs.btrfs -f ${TARGET_DISK}2
 
-echo "\n### Setting up encrypted physical volume"
+echo "\n"
+echo "### Setting up encrypted physical volume"
 cryptsetup luksFormat "$disk2"
 cryptsetup open --type luks "$disk2" lvm
 pvcreate --dataalignment 1m /dev/mapper/lvm
 vgcreate vg0 /dev/mapper/lvm
 
-echo "\n### Creating logical volumes for home/root within encrypted physical volume..."
+echo "\n"
+echo "### Creating logical volumes for home/root within encrypted physical volume..."
 lvcreate -L ${TARGET_ROOT_SIZE} vg0 -n lv-root 
 lvcreate -L ${TARGET_SWAP_SIZE} vg0 -n lv-swap
 lvcreate -l 100%FREE vg0 -n lv-home
 
-echo "\n### Activating logic volumes..."
+echo "\n"
+echo "### Activating logic volumes..."
 modprobe dm_mod # Load necessary modules into memory
 vgscan # Check that kernel realizes that we just read in an LVM 
 yes | vgchange -ay # Activate logical volume groups
 
-echo "\n### Setting filesystem for root/home/swap logical volumes..."
+echo "\n"
+echo "### Setting filesystem for root/home/swap logical volumes..."
 yes | mkfs.btrfs /dev/vg0/lv-root
 yes | mkfs.btrfs /dev/vg0/lv-home
 yes | mkfs.btrfs /dev/vg0/lv-swap
 mkswap /dev/vg0/lv-swap
 swapon /dev/vg0/lv-swap
 
-echo "\n### Mounting partitions..."
+echo "\n"
+echo "### Mounting partitions..."
 mkdir -p /mnt/gentoo
 mkdir -p /mnt/boot && mount ${TARGET_DISK}1 /mnt/boot
 mkdir -p /mnt/gentoo/root && mount /dev/vg0/lv-root /mnt/gentoo/root
 mkdir -p /mnt/gentoo/home && mount /dev/vg0/lv-home /mnt/gentoo/home
 mkdir -p /mnt/gentoo/efi && mount $disk1 /mnt/gentoo/efi
 
-echo "\n### Downloading and unpacking stage3 tarball"
+echo "\n"
+echo "### Downloading and unpacking stage3 tarball"
 cd /mnt/gentoo
 # Stage 3 with openrc
 if [[ $GENTOO_ARCH == "amd64" ]]; then 
@@ -91,24 +97,24 @@ fi
 tar xpvf stage3-*.tar.xz --xattrs-include='*.*' --numeric-owner
 wait
 
-echo "\n### Configuring make.conf"
+echo "\n"
+echo "### Configuring make.conf"
 make_conf="/mnt/gentoo/etc/portage/make.conf"
 touch $make_conf
-echo "COMMON_FLAGS="$base_flags"" > $make_conf
-echo "CFLAGS="${COMMON_FLAGS}"" > $make_conf
-echo "CXXFLAGS="${COMMON_FLAGS}"" > $make_conf
-echo "MAKEOPTS="-j"" > $make_conf
+echo "COMMON_FLAGS="$base_flags"" >> $make_conf
+echo "CFLAGS="${COMMON_FLAGS}"" >> $make_conf
+echo "CXXFLAGS="${COMMON_FLAGS}"" >> $make_conf
+echo "MAKEOPTS="-j"" >> $make_conf
+echo "GENTOO_MIRRORS="https://mirrors.mit.edu/gentoo-distfiles/ http://www.gtlib.gatech.edu/pub/gentoo"" >> /mnt/gentoo/etc/portage/make.conf
 
-
-echo "\n### Selecting mirror"
-mirrorselect -i -o >> /mnt/gentoo/etc/portage/make.conf
 wait
 mkdir --parents /mnt/gentoo/etc/portage/repos.conf
 cp /mnt/gentoo/usr/share/portage/config/repos.conf /mnt/gentoo/etc/portage/repos.conf/gentoo.conf
 cp --dereference /etc/resolv.conf /mnt/gentoo/etc/ #  Copy DNS info
 wait
 
-echo "\n### Mounting file system"
+echo "\n"
+echo "### Mounting file system"
 mount --types proc /proc /mnt/gentoo/proc
 mount --rbind /sys /mnt/gentoo/sys
 mount --make-rslave /mnt/gentoo/sys
@@ -118,6 +124,7 @@ mount --bind /run /mnt/gentoo/run
 mount --make-slave /mnt/gentoo/run 
 wait
 
-echo "\n### Entering the new environment"
+echo "\n"
+echo "### Entering the new environment"
 #wget https://github.com/kehali-woldemichael/Linux_Auto-Install/raw/main/gentoo-2.sh && wait
 #chmod +x gentoo-2.sh
