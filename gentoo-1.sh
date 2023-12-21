16b8a9f07cae37977b4f5f29dda85055fb72f738
Kehali B. Woldemichael <kehaliwoldemichael@protonmail.com>
2023-12-21 09:48:49 -0500

    Update gentoo-1.sh

--- gentoo-1.sh
+++ gentoo-1.sh
@@ -126,24 +126,6 @@ echo "\n"
 echo "### Entering the new environment"
 
 # Stage 2 of install
+wget https://github.com/kehali-woldemichael/Linux_Auto-Install/raw/main/gentoo-2.sh
+wait
 chroot /mnt/gentoo /bin/bash 
-source /etc/profile 
-export PS1="(chroot) ${PS1}"
-
-echo "\n"
-echo "### Configuring Portage"
-emerge-webrsync # Installing a Gentoo ebuild repository snapshot from the web
-wait
-emerge --sync # Updating the Gentoo ebuild repository
-wait
-
-echo "\n"
-echo "### Configuring Portage"
-eselect profile set "default/linux/arm64/23.0"
-emerge --ask --verbose --update --deep --newuse @world
-emerge --info | grep ^USE > /etc/portage/make.conf
-
-echo "\n"
-echo "### Setting CPU FLAGS"
-emerge --ask app-portage/cpuid2cpuflags
-echo "*/* $(cpuid2cpuflags)" > /etc/portage/package.use/00cpu-flags
