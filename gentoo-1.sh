7137cabe514cc6cd473a31a61f6e8dd1d0aebb33
Kehali B. Woldemichael <kehaliwoldemichael@protonmail.com>
2023-12-21 07:17:01 -0500

    Update gentoo-1.sh

--- gentoo-1.sh
+++ gentoo-1.sh
@@ -117,6 +117,7 @@ mount --bind /run /mnt/gentoo/run
 mount --make-slave /mnt/gentoo/run 
 
 echo "\n### Entering the new environment"
+wget https://github.com/kehali-woldemichael/Linux_Auto-Install/raw/main/gentoo-1.sh
 wget https://github.com/kehali-woldemichael/Linux_Auto-Install/raw/main/gentoo-2.sh
 chroot /mnt/gentoo /bin/bash 
 
