7e0e277954b32e5d22941f6103d250616ed0f2a8
Kehali B. Woldemichael <kehaliwoldemichael@protonmail.com>
2023-12-21 09:48:12 -0500

    Update gentoo-2.sh

--- gentoo-2.sh
+++ gentoo-2.sh
@@ -1,30 +1,23 @@
 #!/bin/sh
 # Stage 2 of install
 
-#source $PWD/gentoo-1.sh # to get installation information from Stage 1 file
-
-chroot /mnt/gentoo /bin/bash 
 source /etc/profile 
 export PS1="(chroot) ${PS1}"
 
-echo "\n### Preparing for a bootloader"
-mkdir /efi
-mount "$disk1" /efi 
-
-echo "\n### Configuring Portage"
+echo "\n"
+echo "### Configuring Portage"
 emerge-webrsync # Installing a Gentoo ebuild repository snapshot from the web
+wait
 emerge --sync # Updating the Gentoo ebuild repository
+wait
 
-echo "\n### Configuring Portage"
-eselect profile list
-read -p "Select profile: " profile_choice
-eselect profile set "$profile_choice"
+echo "\n"
+echo "### Configuring Portage"
+eselect profile set "default/linux/arm64/23.0"
 emerge --ask --verbose --update --deep --newuse @world
 emerge --info | grep ^USE > /etc/portage/make.conf
 
-echo "\n### Printing current make.conf"
-cat /etc/portage/make.conf
 echo "\n"
-
+echo "### Setting CPU FLAGS"
 emerge --ask app-portage/cpuid2cpuflags
 echo "*/* $(cpuid2cpuflags)" > /etc/portage/package.use/00cpu-flags
