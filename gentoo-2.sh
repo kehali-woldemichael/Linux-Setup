b0c1dc36423d725fc501c71ec543abe8057b7571
Kehali B. Woldemichael <kehaliwoldemichael@protonmail.com>
2024-02-11 10:50:45 -0500

    Update gentoo-2.sh

--- gentoo-2.sh
+++ gentoo-2.sh
@@ -12,22 +12,26 @@ mount /dev/sda1 /efi
 
 echo "\n"
 echo "### Configuring Portage"
-emerge-webrsync # Installing a Gentoo ebuild repository snapshot from the web
-wait
-emerge --sync # Updating the Gentoo ebuild repository
-wait
-
-echo "\n"
-echo "### Configuring Portage"
 mkdir --parents /etc/portage/repos.conf
 cp /usr/share/portage/config/repos.conf /etc/portage/repos.conf/gentoo.conf
 wget https://github.com/kehali-woldemichael/Linux-Setup/raw/main/gentoo-make.conf-lv1
 wget https://github.com/kehali-woldemichael/Linux-Setup/raw/main/gentoo-make.conf-lv2
 wget https://github.com/kehali-woldemichael/Linux-Setup/raw/main/gentoo-make.conf-lv3
 
+# Installing a Gentoo ebuild repository snapshot from the web
+emerge-webrsync 
+wait
+# Identify and select 3 fastest mirror options
 emerge --ask --verbose --oneshot app-portage/mirrorselect
-#mirrorselect -i -o >> /etc/portage/make.conf
+wait
+# Restrict by region? ... country?
+#mirrorselect -s3 -b10 -D 
+echo 'GENTOO_MIRRORS="https://mirrors.mit.edu/gentoo-distfiles/"' >> /etc/portage/make.conf
+# Updating the Gentoo ebuild repository
+emerge --sync --quiet
+wait
 
+#  Choosing the right profile
 eselect profile list | grep -v desktop | grep -v systemd | grep -v musl | grep -v split-usr | grep -v big-endian | grep -v hardened 
 read -p "Profile: " profile
 eselect profile set "$profile"
@@ -39,9 +43,25 @@ eselect profile set "$profile"
 
 echo "\n"
 echo "### Setting CPU FLAGS"
-# (fix) emerge --ask app-portage/cpuid2cpuflags
-# (fix) echo "*/* $(cpuid2cpuflags)" > /etc/portage/package.use/00cpu-flags
+emerge --ask app-portage/cpuid2cpuflags
+echo "*/* $(cpuid2cpuflags)" > /etc/portage/package.use/00cpu-flags
 
+# Setting locale 
 echo "US/Eastern" >> /etc/timezone
+nano /etc/locale.gen
+locale-gen
+eselect locale list
+read -p "Locale #: " locale_target
+eselect locale set $locale_target
+env-update && source /etc/profile && export PS1="(chroot) ${PS1}"
+
+# Kernel Confirguration 
+#emerge --ask sys-kernel/linux-firmware
+# Install kernel sources ... for manual kernel configuration
+emerge --ask sys-kernel/gentoo-sources
+emerge --ask sys-apps/pciutils
+# Open menu-driven configuration screen
+cd /usr/src/linux*
+make menuconfig # follow instructions on https://wiki.gentoo.org/wiki/Handbook:AMD64/Installation/Kernel#Alternative:_Manual_configuration
 
 
